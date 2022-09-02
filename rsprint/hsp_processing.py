a828328ca3d6b9fb1a44cf15db150a1fb719c39b
Francois Charih <francois@charih.ca>
2022-09-02 12:23:00 -0400

    Finish standalone script

--- rsprint/hsp_processing.py
+++ rsprint/hsp_processing.py
@@ -6,11 +6,23 @@ Copyright (c) 2022, François Charih
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
+from loguru import logger
 import Bio.SeqIO
+
 import rsprint
 
 def main(args):
-    print("processing hsps")
+    logger.info("Loading the protein sequences...")
     proteins = [(p.id, str(p.seq)) for p in Bio.SeqIO.parse(args.input, "fasta")]
-    # TODO
 
+    logger.info(f"Loading the HSPs...")
+    hsps = set([tuple(x.split()) for x in open(args.hsps).read().split("\n")])
+
+    logger.info(f"Processing the HSPs to account for overrepresented residues...")
+    processed_hsps = rsprint.process_hsps(proteins, hsps, kmer_size=args.kmer_size, t_count=args.t_count)
+
+    logger.info(f"Writing to a file...")
+    with open(args.output, "w") as output_file:
+        output_file.write("\n".join([f"{x[0]} {x[1]} {x[2]} {x[3]} {x[4]}" for x in processed_hsps]))
+
+    logger.info(f"Done!")
--- rsprint/scoring.py
+++ rsprint/scoring.py
@@ -33,11 +33,27 @@ def score_all_to_all(args):
     logger.info(f"Done!")
 
 def score_peptides(args):
-    print("Scoring peptides")
-    # scores = rsprint.score_interactions()
-    #rsprint.score_peptides
-    #proteins = [(p.id, str(p.seq)) for p in Bio.SeqIO.parse(args.input, "fasta")]
-    #hsps = rsprint.extract_hsps(proteins)
-    #with open(args.output, "w") as output_file:
-    #    output_file.write("\n".join([f"{x[0]} {x[1]} {x[2]} {x[3]} {x[4]}" for x in hsps]))
+    logger.info("Loading the protein sequences...")
+    proteins = [(p.id, str(p.seq)) for p in Bio.SeqIO.parse(args.input, "fasta")]
 
+    logger.info("Loading the peptide sequences...")
+    peptides = [(p.id, str(p.seq)) for p in Bio.SeqIO.parse(args.peptides, "fasta")]
+
+    logger.info(f"Loading the protein HSPs...")
+    hsps = set([tuple(x.split()) for x in open(args.hsps).read().split("\n")])
+
+    logger.info(f"Loading the training pairs...")
+    training_pairs = [tuple(x.rstrip("\n").split()) for x in open(args.training_pairs)]
+
+    logger.info(f"Extracting the peptide hsps...")
+    peptide_hsps = rsprint.extract_peptide_hsps(proteins, peptides, t_smer=args.t_smer, t_hsp=args.t_hsp, kmer_size=args.kmer_size)
+    hsps = hsps.union(peptide_hsps)
+
+    logger.info(f"Scoring the interactions...")
+    scores = rsprint.score_interactions(proteins, hsps, training_pairs, kmer_size=args.kmer_size)
+    predictions = Predictions(scores, proteins)
+
+    logger.info(f"Saving the scores to a {args.output}...")
+    predictions.save(args.output)
+
+    logger.info(f"Done!")
