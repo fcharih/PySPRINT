99bf576bd35fa2ff1ef8e16233b872e98cfea374
Francois Charih <francois@charih.ca>
2022-12-08 22:31:24 -0500

    Update name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    name="rsprint",
+    name="pysprint",
     version="1.0",
     rust_extensions=[RustExtension("rsprint.rsprint", binding=Binding.PyO3)],
     packages=["rsprint"],
@@ -10,7 +10,7 @@ setup(
     zip_safe=False,
     entry_points={
     'console_scripts': [
-        'rsprint = rsprint.entrypoints:main'
+        'pysprint = rsprint.entrypoints:main'
      ],
     },
     install_requires=[
